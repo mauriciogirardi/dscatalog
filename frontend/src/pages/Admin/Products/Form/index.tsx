27003bd4ed06479afbdafa7becc3c359532e47c3
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-01-30 20:16:47 -0300

    editing products

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, Flex, Text, useToast } from '@chakra-ui/react';
+import { Box, Button, Flex, Spinner, Text, useToast } from '@chakra-ui/react';
+import { useCallback, useEffect, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 import { Textarea } from 'components/Form/Textarea';
-import { useState } from 'react';
 import { Product } from 'types/product';
 import { Select } from 'components/Form/Select';
 import { Input } from 'components/Form/Input';
@@ -20,13 +20,18 @@ const formSchema = yup.object().shape({
 
 export const Form = () => {
     const toast = useToast();
+    const { productId } = useParams();
     const navigate = useNavigate();
+    const isEditing = productId !== 'new';
+
     const [isLoading, setIsLoading] = useState(false);
+    const [isLoadingProduct, setIsLoadingProduct] = useState(false);
 
     const {
         register,
         handleSubmit,
         formState: { errors },
+        setValue,
     } = useForm<Product>({
         resolver: yupResolver(formSchema),
     });
@@ -35,26 +40,31 @@ export const Form = () => {
         name,
         description,
         price,
+        categories,
+        imgUrl,
     }) => {
         try {
+            const text = isEditing ? 'editado' : 'cadastrado';
+
             setIsLoading(true);
             const data = {
                 name,
                 description,
                 price: Number(price),
-                categories: [{ id: 3 }],
+                categories: isEditing ? categories : [{ id: 3 }],
                 date: new Date(),
+                imgUrl: isEditing ? imgUrl : '',
             };
             await requestData<Product>({
-                method: 'POST',
-                url: '/products',
+                method: isEditing ? 'PUT' : 'POST',
+                url: isEditing ? `/products/${productId}` : '/products',
                 data,
                 withCredentials: true,
             });
 
             toast({
-                title: 'Produto Cadastrado.',
-                description: `O produto ${name} foi cadastrado com sucesso`,
+                title: `Produto ${text}.`,
+                description: `O produto ${name} foi ${text} com sucesso`,
                 status: 'success',
                 duration: 4000,
                 isClosable: true,
@@ -69,100 +79,158 @@ export const Form = () => {
         }
     };
 
-    return (
-        <Box bg="gray.800" p="8" borderRadius="4">
-            <Text color="gray.500" fontSize="20px">
-                Dados do Produto
-            </Text>
+    const getProduct = useCallback(async () => {
+        try {
+            setIsLoadingProduct(true);
+            const data = await requestData<Product>({
+                url: `/products/${productId}`,
+            });
 
-            <Flex
-                flexDir={['column']}
-                as="form"
-                onSubmit={handleSubmit(onSubmit)}
-            >
-                <Flex mt="5" gap="5" flexDir={['column', 'column', 'row']}>
-                    <Flex
-                        flexDir={['column']}
-                        gap="4"
-                        w={['100%', '100%', '50%']}
-                    >
-                        <Input
-                            placeholder="Nome do produto"
-                            {...register('name')}
-                            name="name"
-                            error={errors.name}
-                        />
-                        <Input
-                            placeholder="Preço do produto"
-                            {...register('price')}
-                            name="price"
-                            error={errors.price}
-                        />
-                        <Select
-                            data={[]}
-                            placeholder="Categorias"
-                            flushed={false}
-                            {...register('categories')}
-                            name="categories"
-                        />
+            if (data) {
+                const { name, price, description, imgUrl, categories } = data;
+
+                setValue('name', name);
+                setValue('price', price);
+                setValue('description', description);
+                setValue('imgUrl', imgUrl);
+                setValue('categories', categories);
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            setIsLoadingProduct(false);
+        }
+    }, [productId, setValue]);
+
+    useEffect(() => {
+        if (isEditing) {
+            getProduct();
+        }
+    }, [isEditing, getProduct]);
+
+    return (
+        <>
+            {isLoadingProduct ? (
+                <Flex w="100%" mt="20" align="center" justify="center">
+                    <Spinner
+                        thickness="4px"
+                        speed="0.65s"
+                        emptyColor="gray.200"
+                        color="blue.500"
+                        size="xl"
+                    />
+                </Flex>
+            ) : (
+                <>
+                    <Box bg="gray.800" p="8" borderRadius="4">
+                        <Text color="gray.500" fontSize="20px">
+                            Dados do Produto
+                        </Text>
 
                         <Flex
-                            align={['flex-start', 'center']}
-                            flexDir={['column', 'row']}
-                            gap={['3', '0']}
+                            flexDir={['column']}
+                            as="form"
+                            onSubmit={handleSubmit(onSubmit)}
                         >
-                            <Button colorScheme="green" size="sm" w="150px">
-                                Adicionar Imagem
-                            </Button>
-                            <Text
-                                ml={['0', '5']}
-                                fontSize="12px"
-                                w={['100%', '200px']}
-                                color="gray.500"
+                            <Flex
+                                mt="5"
+                                gap="5"
+                                flexDir={['column', 'column', 'row']}
                             >
-                                As imagens devem ser JPG ou PNG e não devem
-                                ultrapassar 5 mb.
-                            </Text>
+                                <Flex
+                                    flexDir={['column']}
+                                    gap="4"
+                                    w={['100%', '100%', '50%']}
+                                >
+                                    <Input
+                                        placeholder="Nome do produto"
+                                        {...register('name')}
+                                        name="name"
+                                        error={errors.name}
+                                    />
+                                    <Input
+                                        placeholder="Preço do produto"
+                                        {...register('price')}
+                                        name="price"
+                                        error={errors.price}
+                                    />
+                                    <Select
+                                        data={[]}
+                                        placeholder="Categorias"
+                                        flushed={false}
+                                        {...register('categories')}
+                                        name="categories"
+                                    />
+
+                                    <Flex
+                                        align={['flex-start', 'center']}
+                                        flexDir={['column', 'row']}
+                                        gap={['3', '0']}
+                                    >
+                                        <Button
+                                            colorScheme="green"
+                                            size="sm"
+                                            w="150px"
+                                        >
+                                            Adicionar Imagem
+                                        </Button>
+                                        <Text
+                                            ml={['0', '5']}
+                                            fontSize="12px"
+                                            w={['100%', '200px']}
+                                            color="gray.500"
+                                        >
+                                            As imagens devem ser JPG ou PNG e
+                                            não devem ultrapassar 5 mb.
+                                        </Text>
+                                    </Flex>
+                                </Flex>
+
+                                <Box w={['100%', '100%', '50%']}>
+                                    <Textarea
+                                        placeholder="Descrição"
+                                        borderColor="gray.400"
+                                        h="152px"
+                                        {...register('description')}
+                                        name="description"
+                                        error={errors.description}
+                                        resize="none"
+                                    />
+                                </Box>
+                            </Flex>
+
+                            <Flex
+                                gap="5"
+                                w="100%"
+                                mt="8"
+                                justify={['flex-end']}
+                            >
+                                <Button
+                                    w={['50%', '150px']}
+                                    size="sm"
+                                    colorScheme="red.200"
+                                    variant="outline"
+                                    onClick={() => navigate(-1)}
+                                    _hover={{
+                                        color: 'red.300',
+                                    }}
+                                >
+                                    Cancelar
+                                </Button>
+                                <Button
+                                    w={['50%', '150px']}
+                                    size="sm"
+                                    colorScheme="facebook"
+                                    type="submit"
+                                    isLoading={isLoading}
+                                >
+                                    {isEditing ? 'Editar' : 'Salvar'}
+                                </Button>
+                            </Flex>
                         </Flex>
-                    </Flex>
-
-                    <Box w={['100%', '100%', '50%']}>
-                        <Textarea
-                            placeholder="Descrição"
-                            borderColor="gray.400"
-                            h="152px"
-                            {...register('description')}
-                            name="description"
-                            error={errors.description}
-                            resize="none"
-                        />
                     </Box>
-                </Flex>
-
-                <Flex gap="5" w="100%" mt="8" justify={['flex-end']}>
-                    <Button
-                        w={['50%', '150px']}
-                        size="sm"
-                        colorScheme="red.200"
-                        variant="outline"
-                        onClick={() => navigate(-1)}
-                        _hover={{
-                            color: 'red.300',
-                        }}
-                    >
-                        Cancelar
-                    </Button>
-                    <Button
-                        w={['50%', '150px']}
-                        size="sm"
-                        colorScheme="facebook"
-                        type="submit"
-                        isLoading={isLoading}
-                    >
-                        Salvar
-                    </Button>
-                </Flex>
-            </Flex>
-        </Box>
+                </>
+            )}
+        </>
     );
 };
--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -12,80 +12,100 @@ import { ButtonBackLink } from 'components/ButtonBackLink';
 import requestData from 'api/requests';
 
 export default function ProductsDetails() {
-  const [product, setProduct] = useState({} as Product);
-  const [loading, setLoading] = useState(false);
-  const { id } = useParams();
+    const [product, setProduct] = useState({} as Product);
+    const [loading, setLoading] = useState(false);
+    const { id } = useParams();
 
-  useEffect(() => {
-    async function getProduct() {
-      setLoading(true);
-      const data = await requestData<Product>({ url: `/products/${id}` });
+    useEffect(() => {
+        async function getProduct() {
+            try {
+                setLoading(true);
+                const data = await requestData<Product>({
+                    url: `/products/${id}`,
+                });
 
-      if (data) {
-        setProduct(data);
-        setLoading(false);
-      }
-    }
+                if (data) {
+                    setProduct(data);
+                }
+            } catch (e) {
+                console.log(e);
+            } finally {
+                setLoading(false);
+            }
+        }
 
-    getProduct();
-  }, [id]);
+        getProduct();
+    }, [id]);
 
-  return (
-    <>
-      <Header />
+    return (
+        <>
+            <Header />
 
-      <Flex
-        height={['90vh', '92.5vh']}
-        maxW="900px"
-        align="center"
-        w="100%"
-        m="auto"
-        px="4"
-      >
-        {loading && <SkeletonProductDetail />}
+            <Flex
+                height={['90vh', '92.5vh']}
+                maxW="900px"
+                align="center"
+                w="100%"
+                m="auto"
+                px="4"
+            >
+                {loading && <SkeletonProductDetail />}
 
-        {!loading && product && (
-          <Box w="100%" bg="gray.800" borderRadius="4px" p="5" boxShadow="md">
-            <>
-              <ButtonBackLink href={CATALOG} />
+                {!loading && product && (
+                    <Box
+                        w="100%"
+                        bg="gray.800"
+                        borderRadius="4px"
+                        p="5"
+                        boxShadow="md"
+                    >
+                        <>
+                            <ButtonBackLink href={CATALOG} />
 
-              <Divider mb="8" />
+                            <Divider mb="8" />
 
-              <Flex flexDir={['column', 'column', 'row']}>
-                <Box w={['100%', '100%', '49%']}>
-                  <Flex justify={['center', 'center', 'start']}>
-                    <Image
-                      src={product.imgUrl}
-                      alt={product.name}
-                      w={['100%', '80%', '300px']}
-                      objectFit="cover"
-                      borderRadius="4px"
-                    />
-                  </Flex>
-                  <Heading as="h4" fontSize="20" color="gray.100" mt="3">
-                    {product.name}
-                  </Heading>
-                  <Price
-                    amount={product.price}
-                    brlSize="18"
-                    realSize="30"
-                    centSize="18"
-                  />
-                </Box>
+                            <Flex flexDir={['column', 'column', 'row']}>
+                                <Box w={['100%', '100%', '49%']}>
+                                    <Flex
+                                        justify={['center', 'center', 'start']}
+                                    >
+                                        <Image
+                                            src={product.imgUrl}
+                                            alt={product.name}
+                                            w={['100%', '80%', '300px']}
+                                            objectFit="cover"
+                                            borderRadius="4px"
+                                        />
+                                    </Flex>
+                                    <Heading
+                                        as="h4"
+                                        fontSize="20"
+                                        color="gray.100"
+                                        mt="3"
+                                    >
+                                        {product.name}
+                                    </Heading>
+                                    <Price
+                                        amount={product.price}
+                                        brlSize="18"
+                                        realSize="30"
+                                        centSize="18"
+                                    />
+                                </Box>
 
-                <Box mt={['5']} w={['100%', '100%', '49%']}>
-                  <Heading as="h4" fontSize="18">
-                    Descrição do Produto
-                  </Heading>
-                  <Text color="gray.100" fontSize="15">
-                    {product.description}
-                  </Text>
-                </Box>
-              </Flex>
-            </>
-          </Box>
-        )}
-      </Flex>
-    </>
-  );
+                                <Box mt={['5']} w={['100%', '100%', '49%']}>
+                                    <Heading as="h4" fontSize="18">
+                                        Descrição do Produto
+                                    </Heading>
+                                    <Text color="gray.100" fontSize="15">
+                                        {product.description}
+                                    </Text>
+                                </Box>
+                            </Flex>
+                        </>
+                    </Box>
+                )}
+            </Flex>
+        </>
+    );
 }
